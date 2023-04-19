7558903556d29637525eb25319d1992897c4fe96
Klimbutsky Kostya <pravednik2008@ukr.net>
2023-04-19 19:46:35 +0300

    git@github.com:KostyaKLiM/React-App.git~

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import style from'./Footer.module.scss';
+
+function Footer() {
+  return (
+   <>
+   <h1 className={style.colorText}>Footer</h1>
+   </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import style from'./Header.module.scss';
+
+function Header() {
+  return (
+   <>
+   <div className={style.colorText}>Header</div>
+   </>
+  );
+}
+
+export default Header;
