8935555d73767c937d0e89854f94b57f262762f4
Klimbutsky Kostya <pravednik2008@ukr.net>
2023-04-26 21:39:39 +0300

    add files

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,28 @@
-import LogoIcon from '../icons/logoicon';
-import'./Login.scss';
+import "./Login.scss";
 
 function Login() {
   return (
-   <>
-<div className='login'>
-</div>
-   </>
+    <>
+      <div className="login">
+        <div className="login__title">NFT Access</div>
+        <div className="login__desc">
+          Please fill your detail to access your account.
+        </div>
+        <div className="input">
+          <span> Email </span>
+          <input className="input__email" type="email" placeholder="debra.holt@example.com"/>
+          <span>Password</span>
+          <input className="input__password" type="password" placeholder="••••••••"/>
+        </div>
+        <div className="checkbox">
+          <label className="checkbox__block">
+          <input className="checkbox-custom" type="checkbox"/>
+          <div className="remember">Remember me</div>
+          </label>
+          <a href="/" className="forgot">Forgot Password?</a>
+        </div>
+      </div>
+    </>
   );
 }
 
